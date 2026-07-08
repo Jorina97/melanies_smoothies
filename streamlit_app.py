fedb372015505fb0127b8493b23bf7abd1d61dd0
Jorina97 <58344613+Jorina97@users.noreply.github.com>
2026-07-08 09:20:43 +0200

    Update streamlit_app.py
    json in een dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
+# New section to display smoothiefruit nutrition information
 import requests  
-# smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothieroot_response.json(), use_container_width=True)
